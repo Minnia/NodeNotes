cefa8c2b5bfd1cd79850a92e2c02e2e0093a5275
Minnia <jasminestockholm@gmail.com>
2020-01-05 20:35:10 +0100

    remove not used code

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,9 +1,5 @@
 const fs = require("fs");
 
-const readFile = fs.readFileSync("1-json.json");
-const parsedJSON = JSON.parse(readFile);
-
-console.log(parsedJSON);
 const newInfo = fs.readFileSync("1-json.json");
 const user = JSON.parse(newInfo);
 user.name = "Jasmine";
